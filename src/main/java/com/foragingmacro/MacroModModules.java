56189d4636d47effcd686374d09ffad8bf017793
AverageBozo <averagebozo@proton.me>
2023-02-13 21:03:32 +0100

    Set the modid to "foragingxpdisplay"

--- src/main/java/com/foragingmacro/MacroModModules.java
+++ src/main/java/com/foragingmacro/MacroModModules.java
@@ -5,7 +5,7 @@ import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = "foragingmacro", name = "Foraging Macro", version = "2.4.1", clientSideOnly = true, acceptedMinecraftVersions = "[1.8.9]")
+@Mod(modid = "foragingxpdisplay", name = "Foraging Macro", version = "2.4.1", clientSideOnly = true, acceptedMinecraftVersions = "[1.8.9]")
 public class MacroModModules {
 
     @Mod.EventHandler
